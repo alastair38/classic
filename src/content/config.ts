e8aac732154db050ef68b3e73a75048137ce7cf0
alastair38 <alastair@alastaircox.com>
2024-04-17 09:04:35 +0100

    First commit

---
+++ src/content/config.ts
@@ -0,0 +1,168 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection, reference } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const pagesCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      featuredImg: z
+        .object({
+          image: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      hero: z
+        .object({
+          heading: z.string().optional(),
+          byline: z.string().optional(),
+          shadow: z.boolean().optional(),
+          fullWidth: z.boolean().optional(),
+          duotone: z.boolean().optional(),
+          background: z
+            .enum(['primary', 'secondary', 'default', 'dark'])
+            .optional(),
+          img: image(),
+          imgAlt: z.string(),
+          textOnly: z.boolean().optional(),
+          showLogo: z.boolean().optional(),
+          linkText: z.string().optional(),
+          linkStyle: z.string().optional(),
+          linkTarget: z.string().optional(),
+          textPosition: z.string().optional(),
+          imgSize: z.string().optional(),
+
+          textStyle: z.string().optional(),
+        })
+        .optional(),
+      showcase: z
+        .object({
+          heading: z.string().optional(),
+          layout: z.string().optional(),
+          items: z.number().optional(),
+        })
+        .optional(),
+      iconCTA: z
+        .object({
+          title: z.string().optional(),
+          byline: z.string().optional(),
+          layout: z.enum(['horizontal', 'vertical']).optional(),
+          shape: z.enum(['rounded', 'square']).optional(),
+          border: z.boolean().optional(),
+          link: z.string().optional(),
+          icons: z
+            .object({
+              one: z.string().optional(),
+              two: z.string().optional(),
+              three: z.string().optional(),
+            })
+            .optional(),
+        })
+        .optional(),
+      imageCTA: z
+        .array(
+          z.object({
+            title: z.string().optional(),
+            byline: z.string().optional(),
+            layout: z.enum(['horizontal', 'vertical']).optional(),
+            shape: z.enum(['rounded', 'square']).optional(),
+            border: z.boolean().optional(),
+            image: z.string().optional(),
+            background: z
+              .enum(['primary', 'secondary', 'default', 'dark'])
+              .optional(),
+            link: z.string().optional(),
+          })
+        )
+        .optional(),
+      quote: z
+        .object({
+          text: z.string().optional(),
+          citation: z.string().optional(),
+          weight: z.enum(['light', 'medium', 'heavy']).optional(),
+          style: z.enum(['normal', 'capital', 'upper']).optional(),
+          iconColor: z.enum(['primary', 'secondary', 'tertiary']).optional(),
+        })
+        .optional(),
+    }),
+});
+
+const servicesCollection = defineCollection({
+  type: 'data', // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      icon: z.string().optional(),
+      services: z.array(z.string()).optional(),
+      image: image().optional(),
+      alt: z.string().optional(),
+      featured: z.boolean().optional(),
+    }),
+});
+
+const peopleCollection = defineCollection({
+  type: 'data', // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      biography: z.string(),
+      title: z.string(),
+      photo: z.object({
+        image: image(),
+        alt: z.string(),
+      }),
+      socialLinks: z
+        .array(
+          z.object({
+            title: z.string().optional(),
+            url: z.string().optional(),
+            icon: z.string().optional(),
+          })
+        )
+        .optional(),
+      order: z.number().default(1),
+    }),
+});
+
+const mainNavCollection = defineCollection({
+  type: 'data', // v2.5.0 and later
+  schema: () =>
+    z.object({
+      title: z.string(),
+      link: z.string(),
+      order: z.number(),
+    }),
+});
+
+const settingsCollection = defineCollection({
+  type: 'data', // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      byline: z.string(),
+      logoSVG: z.string(),
+      socialImage: z
+        .object({
+          image: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      socialLinks: z
+        .array(
+          z.object({ title: z.string(), url: z.string(), icon: z.string() })
+        )
+        .optional(),
+    }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  pages: pagesCollection,
+  services: servicesCollection,
+  people: peopleCollection,
+  navigation: mainNavCollection,
+  settings: settingsCollection,
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: ['-left-4', '-left-8'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      dark: colors.black,
+      light: colors.white,
+      base: colors.slate,
+      tertiary: colors.emerald,
+      secondary: colors.violet,
+      primary: colors.yellow,
+      pink: colors.fuchsia,
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      circle: 'circle',
+      dash: '-',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/mazey.svg')",
+      },
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        main: 'minmax(0.75rem, 3rem) 1fr minmax(0.75rem, 3rem)',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+};
